836fa5684c4c14ff1ba1667c374ee2e4b7885d31
ellis882 <ellisrachel1304@me.com>
2022-01-13 09:50:36 +0000

    installed allauth and pillow and copied allauth templates

--- charlies/urls.py
+++ charlies/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include("restaurant.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
