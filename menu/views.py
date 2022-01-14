60ce35cc6384c983dd5cc1c1c68f209902f111fd
ellis882 <ellisrachel1304@me.com>
2022-01-14 12:19:15 +0000

    add forloop.counter and forloop.first to template list.html

--- menu/views.py
+++ menu/views.py
@@ -6,16 +6,17 @@ def meals_list(request):
     """
     get each meal in the menu with description and price
     in different category that can be managed at the
-    list.html page
+    list.html page. Convert specials to list to
+    iterate many times
     """
     meals_list = Menu.objects.all()
     categories = Category.objects.all()
-    specials = Specials.objects.all()
+    specials = list(Specials.objects.all())
 
     context = {
         'meals_list': meals_list,
         'categories': categories,
-        'specials': specials
+        'specials': specials,
     }
 
     return render(request, 'menu/list.html', context)
